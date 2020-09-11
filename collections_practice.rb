b0ac3a47ae18271e8a050c9ee5d4beeacf4a9580
Dasha Lary <dk2800@nyu.edu>
2020-09-11 13:58:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -43,5 +43,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect { |element, index| element + "s" if element != element[1] }
+  array.except(2).each_with_index.collect { |element, index| element + "s" }
 end
