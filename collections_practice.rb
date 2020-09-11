3d049f76a8fc68f48b3fef0c982adbe86329a791
Dasha Lary <dk2800@nyu.edu>
2020-09-11 13:36:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(array)
+array.sort do |a, b|
+a <=> b 
+end
+end
 
+def sort_array_desc(array)
+array.sort.reverse do |a, b|
+a <=> b 
+end 
+end
+
+def sort_array_char_count(array)
+array.sort do |a, b|
+  a.length <=> b.length
+end
+end
+
+def swap_elements(array)
+array[1], array[2] = array[2], array[1]
+return array
+end
+
+def reverse_array(array)
+array.reverse
+end
+
+def kesha_maker(array)
+  newarray = []
+array.each do |string|
+ string[2] = "$"
+ newarray << string 
+end
+return newarray
+end
+
+def find_a(array)
+array.find { |string| 
+  
+end
